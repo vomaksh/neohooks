a9b0e864bd5ffa6d058e26399bd4df7bf4ceddb0
iyorozuya <iyaksha@tutanota.com>
2021-12-31 20:13:02 +0530

    fixes and improvements in welcome page, next is request lists

--- src/routes/webhook/WebhookInfo.tsx
+++ src/routes/webhook/WebhookInfo.tsx
@@ -1,10 +1,23 @@
-import { Box, Button, Flex, Heading, HStack, Text, useClipboard, VStack } from '@chakra-ui/react';
-import { GoClippy } from 'react-icons/go';
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  Text,
+  Tooltip,
+  useClipboard,
+  useColorModeValue,
+  VStack,
+} from '@chakra-ui/react';
 import { toast } from 'react-hot-toast';
+import { GoClippy } from 'react-icons/go';
+import { MdOpenInNew } from 'react-icons/md';
 
 export function WebhookInfo() {
   const webhookURL = 'https://webhook.site/b9d10e34-b83a-4b2d-9ae0-154d31669fca';
   const { onCopy } = useClipboard(webhookURL);
+  const webhookURLBgColor = useColorModeValue('gray.100', 'gray.700');
   return (
     <Flex flex={1} bgColor="whiteAlpha.30" padding={4} height="full">
       <VStack spacing={4}>
@@ -23,30 +36,38 @@ export function WebhookInfo() {
           <Box width="full">
             <Heading fontSize="2xl">Webhook URL</Heading>
           </Box>
-          <HStack
-            display="flex"
-            alignItems="center"
-            bgColor="gray.100"
-            rounded="base"
-            px={4}
-            py={1.5}
-          >
-            <Text fontSize="base">
+          <HStack display="flex" alignItems="center" spacing={2}>
+            <Text fontSize="base" bgColor={webhookURLBgColor} py={1} px={3} rounded="base">
               <pre>{webhookURL}</pre>
             </Text>
-            <Button
-              size="sm"
-              fontSize="base"
-              onClick={(e) => {
-                e.preventDefault();
-                onCopy();
-                toast.success('Copied URL 👍', {
-                  position: 'bottom-center',
-                });
-              }}
-            >
-              <GoClippy />
-            </Button>
+            <HStack spacing={2}>
+              <Tooltip label="Copy to clipboard">
+                <Button
+                  size="sm"
+                  fontSize="base"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    onCopy();
+                    toast.success('Copied URL 👍', {
+                      position: 'bottom-center',
+                    });
+                  }}
+                >
+                  <GoClippy />
+                </Button>
+              </Tooltip>
+              <Tooltip label="Open in new tab">
+                <Button
+                  size="sm"
+                  fontSize="base"
+                  onClick={(e) => {
+                    e.preventDefault();
+                  }}
+                >
+                  <MdOpenInNew />
+                </Button>
+              </Tooltip>
+            </HStack>
           </HStack>
         </VStack>
       </VStack>
