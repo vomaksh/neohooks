c161330aa7040f50f8a890382fd05affd932b4be
iyorozuya <iyaksha@tutanota.com>
2022-01-21 07:39:12 +0530

    chore: changed code structure

--- api/main.go
+++ api/main.go
@@ -1,12 +1,14 @@
-package api
+package main
 
 import (
 	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-redis/redis/v8"
 	"github.com/iyorozuya/neohooks/api/controllers"
+	"github.com/iyorozuya/neohooks/api/db"
 	"github.com/iyorozuya/neohooks/api/services"
 	"github.com/iyorozuya/neohooks/api/structs"
 )
@@ -15,7 +17,40 @@ type Controller struct {
 	routes func() []structs.Route
 }
 
-func Bootstrap(db *redis.Client, r *chi.Mux) {
+func main() {
+	r := chi.NewRouter()
+
+	// Base middlewares
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	// json, xml, plain text allowed as content type
+	r.Use(middleware.AllowContentType("application/json", "application/xml", "text/plain"))
+
+	// Middleware for setting header Content-Type to application/json
+	r.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Add("Content-Type", "application/json")
+			next.ServeHTTP(w, r)
+		})
+	})
+
+	// Redis client
+	rdb := db.Connect(db.DBOptions{
+		Addr:     "redis:6379",
+		Name:     0,
+		Password: "",
+	})
+
+	// bootstrap api
+	bootstrap(rdb, r)
+	if err := http.ListenAndServe(":5000", r); err != nil {
+		log.Fatalf("%s\n", err)
+	}
+}
+
+func bootstrap(db *redis.Client, r *chi.Mux) {
 	// Initialize all services
 	webhookRequestService := services.WebhookRequestService{DB: db}
 	webhookService := services.WebhookService{
