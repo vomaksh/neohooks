cfd957760e9eaf7a1c23abce5b2d9e9452460daf
iyorozuya <iyaksha@tutanota.com>
2022-01-23 02:32:31 +0530

    test: added loader init test case

--- ui/src/routes/home/Loader.tsx
+++ ui/src/routes/home/Loader.tsx
@@ -9,7 +9,7 @@ export function Loader() {
       <Flex direction="column" flex={2} alignItems="center" justifyContent="center">
         <Image src={HookImage} alt="hook" height={150} />
         <Box height={8} />
-        <Spinner thickness="4px" size="xl" color="green.400" />
+        <Spinner data-testid="home-loader" thickness="4px" size="xl" color="green.400" />
       </Flex>
       <Box flex={1} />
     </Flex>
